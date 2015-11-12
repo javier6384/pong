dc84be3de3dfd0db69754277e4ebf631260cc94d
1DAW16 <1DAW16@1DAW16>
2015-11-12 10:34:14 +0100

    Funciona, los objetos se han cambiado de ubicación, se están asignando constantes y variables, y hay que asignar los diferentes golpeos de las palas.

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -10,10 +10,8 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -28,15 +26,17 @@ public class Pong extends Application {
     
     final int WORLD_WIDTH = 500;
     final int WORLD_HEIGHT = 400;
-  
-    int posYJugador1 = 0;
-    int posYJugador2 = 0;
+    final int RIGHT_LIMIT = 0;
+    final int TOP_LIMIT = 0;
     
-    final int LARGO_JUGADORES = 70;
-    final int ANCHO_JUGADORES = 10;
+    int posIniJ1 = 165;
+    int posIniJ2 = 165;
     
-    final int LINEA_JUGADOR1 = 20;
-    final int LINEA_JUGADOR2 = 470;
+    final int HEIGHT_PLAYERS = 70;
+    final int WIDTH_PLAYERS = 10;
+    
+    final int POSX_J1 = 20;
+    final int POSX_J2 = 470;
             
     String movLateral = "derecha";
     String movVertical = "arriba";
@@ -48,13 +48,13 @@ public class Pong extends Application {
     byte movimientoJugador2 = 0;
     
     //Jugador 1
-    Rectangle jugador1 = new Rectangle (LINEA_JUGADOR1,165,ANCHO_JUGADORES,LARGO_JUGADORES);
+    Rectangle jugador1 = new Rectangle (0,0,WIDTH_PLAYERS,HEIGHT_PLAYERS);
 //        jugador1.setFill(Color.WHITE);
-    double posJugador1 = jugador1.getTranslateY();
+    double posJugador1;
     
     //jugador 2
-    Rectangle jugador2 = new Rectangle (LINEA_JUGADOR2,165,ANCHO_JUGADORES,LARGO_JUGADORES);
-    double posJugador2 = jugador2.getTranslateY();
+    Rectangle jugador2 = new Rectangle (0,0,WIDTH_PLAYERS,HEIGHT_PLAYERS);
+    double posJugador2;
     
 //    Image imgShip = new Image(getClass().getResourceAsStream("/image/ship.png"));
 //    Image imgBullet = new Image(getClass().getResourceAsStream("/image/bullet.png"));
@@ -71,18 +71,26 @@ public class Pong extends Application {
         primaryStage.show();
         
         //import javafx.scene.shape.Rectangle
-        Circle bolita = new Circle (250, 200, 10);
+        Circle bolita = new Circle (0, 0, 10);
+        bolita.setTranslateX(250);
+        bolita.setTranslateY(200);
         bolita.setFill(Color.WHITE);
         root.getChildren().add(bolita);
         
         //Jugador 1
-//        Rectangle jugador1 = new Rectangle (LINEA_JUGADOR1,165,ANCHO_JUGADORES,LARGO_JUGADORES);
+//        Rectangle jugador1 = new Rectangle (POSX_J1,165,WIDTH_PLAYERS,HEIGHT_PLAYERS);
         jugador1.setFill(Color.WHITE);
+        jugador1.setTranslateX(POSX_J1);
+        jugador1.setTranslateY(posIniJ1);
+        posJugador1 = jugador1.getTranslateY();
         root.getChildren().add(jugador1);
         
         //jugador 2
-//        Rectangle jugador2 = new Rectangle (LINEA_JUGADOR2,165,ANCHO_JUGADORES,LARGO_JUGADORES);
+//        Rectangle jugador2 = new Rectangle (POSX_J2,165,WIDTH_PLAYERS,HEIGHT_PLAYERS);
         jugador2.setFill(Color.WHITE);
+        jugador2.setTranslateX(POSX_J2);
+        jugador2.setTranslateY(posIniJ2);
+        posJugador2 = jugador2.getTranslateY();
         root.getChildren().add(jugador2);
         
         //Línea divisoria
@@ -106,8 +114,8 @@ public class Pong extends Application {
                 double posX = bolita.getTranslateX();
                 double posY = bolita.getTranslateY();
                 System.out.println(posY);
-                System.out.println(posJugador1);
-                System.out.println(posJugador2);
+//                System.out.println(posJugador1);
+//                System.out.println(posJugador2);
 
                 String textMarcadorLocal = String.valueOf(marcadorLocal);
                 String textMarcadorVisitante = String.valueOf(marcadorVisitante);
@@ -115,13 +123,13 @@ public class Pong extends Application {
                 
                 if (movLateral == "derecha"){
                     posX+=2;
-                    if (posX == 240){
+                    if (posX == (WORLD_WIDTH - 10)){
                         movLateral = "izquierda";
                     }
                 }
                 else {if (movLateral == "izquierda"){
                     posX-=2;
-                    if (posX == -240){
+                    if (posX == RIGHT_LIMIT + 10){
                         movLateral = "derecha";
                         }
                     }
@@ -129,13 +137,13 @@ public class Pong extends Application {
                 
                 if (movVertical == "arriba"){
                     posY--;
-                    if (posY == -190){
+                    if (posY == TOP_LIMIT + 10){
                         movVertical = "abajo";
                     }
                 }
                 else {if (movVertical == "abajo"){
                     posY++;
-                    if (posY == 190){
+                    if (posY == 390){
                         movVertical = "arriba";
                         }
                     }
@@ -144,46 +152,41 @@ public class Pong extends Application {
                 bolita.setTranslateX(posX);
                 bolita.setTranslateY(posY);
                                 
-                if (posX == 240){
+                if (posX == (WORLD_WIDTH - 10)){
                     marcadorLocal++;
                 }
-                else {if (posX == -240){
+                else {if (posX == 10){
                     marcadorVisitante++;}
                 }
                 
                 marcador.setText(textMarcadorLocal + "   " + textMarcadorVisitante);
             
- 
                 posJugador1 += movimientoJugador1;
                 jugador1.setTranslateY(posJugador1);
-                
 
                 posJugador2 += movimientoJugador2;
                 jugador2.setTranslateY(posJugador2);
                 
-                if (posJugador1 == -165){
+                if (posJugador1 == 0){
                     movimientoJugador1 = 0;}
-                else {if (posJugador1 == 165){
+                else {if (posJugador1 == (WORLD_HEIGHT - 10)){
                     movimientoJugador1 = 0;}
                 }
                 
-                if (posJugador2 == -165){
+                if (posJugador2 == 0){
                     movimientoJugador2 = 0;}
-                else {if (posJugador2 == 165){
+                else {if (posJugador2 == (WORLD_HEIGHT - 10)){
                     movimientoJugador2 = 0;}
                 }
                 
-                if ((posX == -210) && (posY >= posJugador1 -35) && (posY <=posJugador1 + 35 )){
+                if ((posX == 40) && (posY >= posJugador1) && (posY <=posJugador1 + HEIGHT_PLAYERS )){
                     movLateral ="derecha";
                 }
                 
-                if ((posX == 210) && (posY >= posJugador2 -35) && (posY <=posJugador2 + 35 )){
+                if ((posX == 460) && (posY >= posJugador2) && (posY <=posJugador2 + HEIGHT_PLAYERS )){
                     movLateral = "izquierda";
                 }
-            }
-            
-            
-            
+            }  
             
     }.start();
         
@@ -193,14 +196,14 @@ public class Pong extends Application {
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
                     case S:
-                        if (posJugador1 == -165){
+                        if (posJugador1 == 0){
                             movimientoJugador1 = 0;
                         }
                         else {movimientoJugador1 = -3;
                         }
                         break;    
                     case X:
-                        if (posJugador1 == 165){
+                        if (posJugador1 == (WORLD_HEIGHT - 10)){
                             movimientoJugador1 = 0;
                         }
                         else {movimientoJugador1 = 3;
@@ -210,14 +213,14 @@ public class Pong extends Application {
                         movimientoJugador1 = 0;
                         break;
                     case K:
-                        if (posJugador2 == -165){
+                        if (posJugador2 == 0){
                             movimientoJugador2 = 0;
                         }
                         else {movimientoJugador2 = -3;
                         }
                         break; 
                     case M:
-                        if (posJugador2 == 165){
+                        if (posJugador2 == (WORLD_HEIGHT - 10)){
                             movimientoJugador2 = 0;
                         }
                         else {movimientoJugador2 = 3;
